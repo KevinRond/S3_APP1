f8350dd84f7e1d33e6902f9ddf3c9f0367e8e5cd
KevinRond <ronk2602@usherbrooke.ca>
2023-05-07 13:14:06 -0400

    Chef termine, manque tests

--- MenuFact02/menufact/Chef.java
+++ MenuFact02/menufact/Chef.java
@@ -1,6 +1,12 @@
 package menufact;
 
+import ingredients.Ingredient;
+import ingredients.exceptions.IngredientException;
+import inventaire.Inventaire;
 import menufact.plats.PlatChoisi;
+import menufact.plats.PlatSante;
+import menufact.plats.Recette;
+import menufact.plats.state.*;
 
 public class Chef {
     private String nom;
@@ -16,25 +22,47 @@ public class Chef {
         return this.nom;
     }
     public void setNom(String nom){this.nom = nom;}
-    public PlatChoisi cuisiner(PlatChoisi plat){
-        /*plat.setEtat(new EtatCommande());
+    public PlatChoisi cuisiner(PlatChoisi plat) throws IngredientException {
+        plat.setState(new StateCommande());
         if(verifierIngredient(plat)){
             preparer(plat);
             terminer(plat);
-            return servir(plat)
+            return servir(plat);
         }
         else{
             return plat;
-        }*/
+        }
+    }
+    private boolean verifierIngredient(PlatChoisi plat) throws IngredientException {
+        Inventaire inventaire = Inventaire.getInstance();
+        Recette recette = plat.getPlat().getRecette();
+
+            for (Ingredient ingredient : recette.getIngredients()){
+                double quantiteRecette = plat.getQuantite() * plat.getPlat().getProportion();
+                double quantiteInventaire = inventaire.getIngredient(ingredient.getNom()).getQuantity();
+
+                if (quantiteInventaire < quantiteRecette){
+                    plat.setState(new StateImpossibleDeServir());
+                    throw new IngredientException("Il manque cette ingredient: " + ingredient);
+                }
+            }
+        return true;
+    }
+    private void preparer(PlatChoisi plat) throws IngredientException {
+        plat.setState(new StateEnPreparation());
+
+        Inventaire inventaire = Inventaire.getInstance();
+        Recette recette = plat.getPlat().getRecette();
+
+        inventaire.utiliserIngredients(recette, plat.getQuantite(), plat.getPlat().getProportion());
+    }
+    private void terminer(PlatChoisi plat){
+        plat.setState(new StateTermine());
+    }
+    private PlatChoisi servir(PlatChoisi plat){
+        plat.setState(new StateServi());
         return plat;
     }
-    private void verifierIngredient(PlatChoisi plat){
-
-    }
-    private void preparer(PlatChoisi plat){
-
-    }
-    private PlatChoisi servir(PlatChoisi plat){return plat;}
     @Override
     public String toString(){return "Chef: {" + "Nom: " + nom + "}";};
 }
